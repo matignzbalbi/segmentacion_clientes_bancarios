5b4cd15319169f8c44baa851202742a9079a7b72
Mat1gnz <mrgnzbalbi@gmail.com>
2025-06-16 18:01:42 -0300

    Modelamiento, Toma de decisiones, más

--- pages/5_5. Evaluación del Modelo.py
+++ pages/5_5. Evaluación del Modelo.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title("Evaluación del Modelo.")
 st.divider()
+st.write("""Como se detallo en 4. Modelamiento, se realizaron dos modelos con K-Prototypes donde uno incluyé **PCA** y el otro no para 
+         las variables numéricas.
+
+En este apartado vamos a detallar los **resultados individuales** de cada modelo y compararlos.
+    """)
+
+
--- pages/7_7. Toma de decisiones.py
+++ pages/7_7. Toma de decisiones.py
@@ -2,3 +2,61 @@ import streamlit as st
 
 st.title("Toma de decisiones.")
 st.divider()
+
+st.header("Segmentación.")
+
+st.markdown('''
+        Debido a las carácteristicas de los clústers generados por KPrototypes proponemos los\
+        siguientes perfiles para cada uno de ellos:
+        * `Cluster 1`: **Clientes X**.
+        * `Cluster 2`: **Clientes Y**.
+        * `Cluster 3`: **Clientes Z**.
+        * `Cluster 4`: **Clientes D**.
+            ''')
+
+st.divider()
+st.header("Estrategias propuestas por tipo de cliente.")
+
+
+st.subheader("Cliente X:")
+st.markdown('''
+- Introducir líneas de productos económicos y ampliar la variedad de artículos con descuentos significativos.
+
+- Personalizar promociones basadas en el comportamiento de compra de este grupo.
+
+- Asegurar que estén informados sobre ofertas a través de campañas de email marketing y newsletters.
+''')
+st.divider()
+
+st.subheader("Cliente Y:")
+st.markdown('''
+- Mejorar la visibilidad de las ofertas en la web y personalizarlas según los intereses del cliente.
+
+- Incrementar la variedad de productos y marcas disponibles.
+
+- Experimentar con descuentos en nuevas categorías para estimular el consumo.
+
+- Fomentar la conversión de clientes regulares en compradores frecuentes mediante una experiencia de usuario optimizada, promociones atractivas y campañas de email marketing o newsletters.
+''')
+st.divider()
+st.subheader("Cliente Z:")
+st.markdown('''
+- Enfocarse en optimizar el catálogo, asegurando que sea claro y accesible.
+
+- Ofrecer beneficios como envíos gratuitos o descuentos en el delivery para incentivar compras en grandes cantidades.
+
+- Incentivar la fidelidad mediante programas de membresía o beneficios post-compra.
+
+- Que los productos sean frescos puede ser un factor clave para que sigan consumiendo
+''')
+st.divider()
+st.subheader("Cliente D:")
+st.markdown('''
+- Crear promociones exclusivas y diferenciadas, enfocadas en productos de alta demanda para este segmento.
+
+- Incentivar la fidelidad mediante programas de membresía o beneficios post-compra.
+
+- Ofrecer envíos gratuitos o descuentos en el delivery para aumentar la conveniencia.
+
+''')
+
