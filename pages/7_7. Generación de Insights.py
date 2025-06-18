56feeb56faaef2e8ac4c2d56de0cb12f0bb1a012
JulianLivolsi <julianlivolsi@outlook.com>
2025-06-17 21:54:39 -0300

    .

--- pages/7_7. Generación de Insights.py
+++ pages/7_7. Generación de Insights.py
@@ -4,3 +4,6 @@ st.set_page_config(layout="wide")
 st.title("Generación de Insights.")
 st.divider()
 
+st.header("Resultados.")
+st.write("En base a la evaluación vista en la solapa anterior vemos que la mejor opcón es utilizar K-means.")
+st.write("Creamos cuatro clusters, y podemos visualizarlos de esta manera:" )
