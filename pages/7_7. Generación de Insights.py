8c4ba22ee0c9efd6662b4f6defff8baf2d8046e8
JulianLivolsi <julianlivolsi@outlook.com>
2025-06-17 21:59:18 -0300

    .

--- pages/7_7. Generación de Insights.py
+++ pages/7_7. Generación de Insights.py
@@ -5,5 +5,5 @@ st.title("Generación de Insights.")
 st.divider()
 
 st.header("Resultados.")
-st.write("En base a la evaluación vista en la solapa anterior vemos que la mejor opcón es utilizar K-means.")
-st.write("Creamos cuatro clusters, y podemos visualizarlos de esta manera:" )
+st.write("En base a la evaluación vista en la solapa anterior vemos que no hay mucha diferencia entre los resultados de los modelos. Por esta razón, decidimos utilizar `K-Prototypes` con `PCA`, ya que en este modelo, estamos teniendo en cuenta todas las variables.")
+st.write("El resultado que obtuvimos fueron cuatro clusters, y podemos visualizarlos de esta manera:" )
