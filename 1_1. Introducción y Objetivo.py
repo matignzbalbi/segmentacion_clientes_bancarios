94372dce09c88b90174a644831739e20afae67ca
JulianLivolsi <julianlivolsi@outlook.com>
2025-06-20 18:38:11 -0300

    .

--- 1_1. Introducción y Objetivo.py
+++ 1_1. Introducción y Objetivo.py
@@ -4,7 +4,7 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
 
 st.markdown("<h1 style='color:#00BFFF;'>Trabajo Final - Laboratorio de Ciencia de Datos</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='color:#ffffff;'>Grupo 2 – Clientes bancarios.</h3>", unsafe_allow_html=True)
+st.markdown("<h2 style='color:#ffffff;'>Grupo 2 – Clientes bancarios.</h2>", unsafe_allow_html=True)
 
 
 st.title("Trabajo Final - Laboratorio de Ciencia de Datos.")
@@ -12,10 +12,12 @@ st.header("Grupo 2 – Clientes bancarios.")
 st.divider()
 
 st.subheader("Contexto.")
+st.markdown("### 📌 Contexto")
 st.write("Una institución bancaria quiere entender mejor el comportamiento de sus clientes para ofrecer\
          servicios personalizados y mejorar la retención.")
 
 st.subheader("Objetivo.")
+st.markdown("### 🎯 Objetivo")
 st.write("Analizar el comportamiento financiero de los clientes y proponer una\
          forma de agruparlos en perfiles útiles para el negocio. De esta forma\
          podemos decir que estaremos trabajando en un modelo NO supervisado, específicamente en un modelo de ‘Clustering’.")
@@ -27,5 +29,7 @@ st.divider()
 st.markdown("Alumnos: **Julian Livolsi, Tomas Martin, Matias Gonzalez Balbi, Tomas Agustin Coll**.")
 
 cols = st.columns(2)
-cols[0].markdown("**Julian Livolsi**  \n**Tomas Martín**")
-cols[1].markdown("**Matias Gonzalez Balbi**  \n**Tomas Agustin Coll**")
+cols[0].markdown("**Julian Livolsi**")
+cols[1].markdown("**Matias Gonzalez Balbi**")
+cols[2].markdown("**Tomas Martín**")
+cols[3].markdown("**Tomas Agustin Coll**")
